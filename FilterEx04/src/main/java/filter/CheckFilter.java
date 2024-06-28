252064a76d4f62a5576d2ee2819cc9cefa5f0c9d
jaeeun <rosa2070@naver.com>
2024-06-28 17:47:32 +0900

    06/28

---
+++ FilterEx04/src/main/java/filter/CheckFilter.java
@@ -0,0 +1,55 @@
+package filter;
+
+import java.io.IOException;
+
+import javax.security.auth.message.callback.PrivateKeyCallback.Request;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpFilter;
+
+/**
+ * Servlet Filter implementation class CheckFilter
+ */
+@WebFilter( urlPatterns = { "/check/*" })
+public class CheckFilter extends HttpFilter implements Filter {
+
+	/**
+	 * @see Filter#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+	 */
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		// TODO Auto-generated method stub
+		// place your code here
+		
+		//System.out.println(request.getParameter("data"));
+		if ( !request.getParameter("data").equals("")) {
+			// pass the request along the filter chain
+			chain.doFilter(request, response);
+		} else {
+			RequestDispatcher dispatcher = request.getRequestDispatcher("error.jsp");
+			dispatcher.forward(request, response);
+		}     
+
+
+	}
+
+	/**
+	 * @see Filter#init(FilterConfig)
+	 */
+	public void init(FilterConfig fConfig) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+}
