0720f4ce39b818dc5aca2a932047d3a3e88b1d96
jaeeun <rosa2070@naver.com>
2024-07-12 11:06:35 +0900

    이메일테스트

--- MailEx01/src/com/exam/mail/MailSender.java
+++ MailEx01/src/com/exam/mail/MailSender.java
@@ -32,13 +32,15 @@ public class MailSender {
       //내용
       // String content = "<html><head></head><body><font color='red'>테스트 내용</font><br/>테스트 내용</body></html>";
       
+      // file / db
       StringBuilder sbHtml = new StringBuilder();
       sbHtml.append("<html>");
       sbHtml.append("<head>");
       sbHtml.append("<meta charset='utf-8' />");
       sbHtml.append("</head>");
       sbHtml.append("<body>");
-      sbHtml.append("<font color='blue'>테스트 내용</font><br />테스트 내용");      
+      sbHtml.append("<font color='blue'>테스트 내용</font><br />테스트 내용");    
+      sbHtml.append("<img src='https://img1.daumcdn.net/thumb/S300x300/?scode=b2_01&x-twg-thumb-fname=http%3A%2F%2Ft1.daumcdn.net%2Fb2%2Fcreative%2F599267%2Fcf2ddbb7cfb62c660875eed741d85002.jpg&TWGServiceId=b2&Expires=1720753200&Signature=8fPfu0joPR4M8ssj5dRZCA28wjA%3D' />");
       sbHtml.append("</body>");
       sbHtml.append("</html>");
       
